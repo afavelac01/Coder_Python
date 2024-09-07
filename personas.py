313405a49e5cd3756d0f3a09c7437f024373f4c6
Anastacio Favela <afave68@gmail.com>
2024-09-06 20:38:31 -0600

    se agrega edad a personas

--- personas.py
+++ personas.py
@@ -1,8 +1,9 @@
 class Persona():
 
-    def __init__(self,nombre,apellido):
+    def __init__(self,nombre,apellido,edad):
         self.nombre=nombre
         self.apellido=apellido
+        self.edad=edad
 
     def saluda(self):
         return f'"Hola soy {self.nombre}'
