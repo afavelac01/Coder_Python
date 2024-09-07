75763ba7fa8deb6756d886d4df2c8bd5175ae9bc
Anastacio Favela <afave68@gmail.com>
2024-09-06 19:06:41 -0600

    Agregando metodo saluda en clase personas

---
+++ personas.py
@@ -0,0 +1,8 @@
+class Persona():
+
+    def __init__(self,nombre,apellido):
+        self.nombre=nombre
+        self.apellido=apellido
+
+    def saluda(self):
+        return f'"Hola soy {self.nombre}'
